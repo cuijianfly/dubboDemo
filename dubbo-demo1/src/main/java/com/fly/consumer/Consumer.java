b5259a8278a685487246eb16c7d0e5b6c51a9a7b
cuijianfly <c767332172@163.com>
2021-04-18 20:04:27 +0800

    新增接口2

---
+++ dubbo-demo1/src/main/java/com/fly/consumer/Consumer.java
@@ -0,0 +1,15 @@
+package com.fly.consumer;
+
+import com.fly.api.DubboService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
+        context.start();
+        DubboService demoService = context.getBean("helloService", DubboService.class); // 获取远程服务代理
+        String hello = demoService.helloDubbo("tom"); // 执行远程方法
+        System.out.println( hello ); // 显示调用结果
+    }
+}
