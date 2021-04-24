6b384f54772f8736f7a9df79f137ac01a24e80d2
cuijianfly <c767332172@163.com>
2021-04-24 18:43:41 +0800

    提交ä¸代码

--- dubbo-demo1/src/main/java/com/fly/consumer/GitTest.java
+++ dubbo-demo1/src/main/java/com/fly/consumer/GitTest.java
@@ -14,5 +14,6 @@ public class Consumer {
         System.out.println("12345678");
         System.out.println("123456789");
         System.out.println("1234567810");
+        System.out.println("1234567811");
     }
 }
