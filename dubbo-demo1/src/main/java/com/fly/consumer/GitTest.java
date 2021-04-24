a83499664133fc0d57812c3d660202d718c43501
cuijianfly <c767332172@163.com>
2021-04-24 15:40:15 +0800

    新建分支 提交了代码

---
+++ dubbo-demo1/src/main/java/com/fly/consumer/GitTest.java
@@ -0,0 +1,12 @@
+package com.fly.consumer;
+
+import com.fly.api.DubboService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+
+    }
+}
