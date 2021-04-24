0063a68664df553a153bf323df19a7ed0bf43ca2
MoCuishle <38983720+cuijianfly@users.noreply.github.com>
2021-04-24 14:15:53 +0800

    Update Consumer.java

--- dubbo-demo1/src/main/java/com/fly/consumer/Consumer.java
+++ dubbo-demo1/src/main/java/com/fly/consumer/Consumer.java
@@ -23,5 +23,8 @@ public class Consumer {
         System.out.println("master 提交了代码");
         
         System.out.println("master git修改了代码");
+        
+        System.out.println("001");
+    }
     }
 }
