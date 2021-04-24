2f69dd716a52bcebcdfff146828cd9bb2753f3c3
MoCuishle <38983720+cuijianfly@users.noreply.github.com>
2021-04-24 15:50:50 +0800

    Update GitTest.java

--- dubbo-demo1/src/main/java/com/fly/consumer/GitTest.java
+++ dubbo-demo1/src/main/java/com/fly/consumer/GitTest.java
@@ -8,5 +8,6 @@ public class Consumer {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("1234");
+        System.out.println("12345");
     }
 }
